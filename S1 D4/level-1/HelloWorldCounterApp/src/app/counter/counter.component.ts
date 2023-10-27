d7af37c4122007cc5d261616613fdbdd9b3bb408
prajit96 <santraprajit@gmail.com>
2023-10-27 13:48:10 +0530

    Angular hello world and Counter app

---
+++ S1 D4/level-1/HelloWorldCounterApp/src/app/counter/counter.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  templateUrl: './counter.component.html',
+  styleUrls: ['./counter.component.css']
+})
+export class CounterComponent {
+  count: number = 0;
+
+  increaseCount() {
+    this.count++;
+  }
+
+  decreaseCount() {
+    if (this.count > 0) {
+      this.count--;
+    }
+  }
+
+  resetCount() {
+    this.count = 0;
+  }
+}
